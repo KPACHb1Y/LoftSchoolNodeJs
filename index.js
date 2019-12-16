9d47069b89be7315b9972b234ddd7fcdd972471a
KPACHb1Y <barb0s9109@mail.ru>
2019-12-16 13:04:21 +0300

    create homework_1

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const fs = require('fs');
 
-const ws = fs.createWriteStream('stream.jpg', {encoding: 'utf8'});
-
-ws.write('First\n');
-ws.write('Second\n');
-ws.end('End');
+fs.readdir('./listsOfText', (err, files) => {
+    if (err) throw err;
+    files.forEach(item => {
+        const newFolder = item.split('.')[0];
+        if(!fs.existsSync('./newLists')) {
+            fs.mkdir('./newLists', err => {
+                if(err) {
+                    console.error(err);
+                }
+            });
+        }
+        if (!fs.existsSync(`./newLists/${item}`)) {
+            fs.mkdir(`./newLists/${newFolder}`, err => {
+                if (err) console.error(err.message);
+            });
+        }
+        fs.copyFile(`./listsOfText/${item}`, `./newLists/${newFolder}/${item}`, err => {
+            if (err) console.error(err.message);
+        });
+    })
+});

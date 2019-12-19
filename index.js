50932237be790006b3fc5490df7d61a659c136b9
KPACHb1Y <barb0s9109@mail.ru>
2019-12-19 17:38:50 +0300

    add new changes homework_1

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
 const fs = require('fs');
 
-fs.readdir('./listsOfText', (err, files) => {
-    if (err) throw err;
-    files.forEach(item => {
-        const newFolder = item.split('.')[0];
-        if(!fs.existsSync('./newLists')) {
-            fs.mkdir('./newLists', err => {
-                if(err) console.error(err);
-            });
-        }
-        
-        if (!fs.existsSync(`./newLists/${item}`)) {
-            fs.mkdir(`./newLists/${newFolder}`, err => {
-                if (err) console.error(err.message);
-            });
-        }
+const files = ['A.txt', 'B.txt', 'C.txt', 'D.txt', 'F.txt', 'G.txt'];
 
-        fs.copyFile(`./listsOfText/${item}`, `./newLists/${newFolder}/${item}`, err => {
-            if (err) console.error(err.message);
+
+if (!fs.existsSync('./listsOfText')) {
+    fs.mkdir('./listsOfText', err => {
+        if (err) throw err;
+        files.forEach(item => {
+            fs.writeFile(`./listsOfText/${item}`, 'hello', err => {
+                if (err) console.error(err);
+            });
         });
-    })
-});
+
+        fs.readdir('./listsOfText', (err, files) => {
+            if (err) throw err;
+
+            if(!fs.existsSync('./newLists')) {
+                fs.mkdir('./newLists', err => {
+                    if(err) console.error(err);
+                });
+            }
+
+            files.forEach(item => {
+                const newFolder = item.split('.')[0];
+                if (!fs.existsSync(`./newLists/${newFolder}`)) {
+                    fs.mkdir(`./newLists/${newFolder}`, err => {
+                        if (err) console.error(err.message);
+                        fs.copyFile(`./listsOfText/${item}`, `./newLists/${newFolder}/${item}`, err => {
+                            if (err) console.error(err.message);
+                        });
+                    });
+                } else {
+                    fs.copyFile(`./listsOfText/${item}`, `./newLists/${newFolder}/${item}`, err => {
+                        if (err) console.error(err.message);
+                    });
+                }
+            });
+        });
+    });
+}
